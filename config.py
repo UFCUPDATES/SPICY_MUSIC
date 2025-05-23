09f5fd82c4509a852f335f4fbf46a1b29ebb4701
ᴧʟᴘʜᴧ ʙᴧʙʏ <168528642+TEAMPURVI@users.noreply.github.com>
2025-05-23 19:10:35 +0530

    ᴘᴜʀᴠɪ ʙᴏᴛs !!

--- config.py
+++ config.py
@@ -30,6 +30,9 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # Get it from http://dashboard.heroku.com/account
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
+API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
+API_KEY = getenv("API_KEY", 'NxGBNexGenBotsad3e6e') # youtube song api key, get it from ht
+
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/UFCUPDATES/SPICY_MUSIC",
